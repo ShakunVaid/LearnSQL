3e730468bd506cc5284f0334155a4e02cbfa9d29
ShakunVaid <98100058+ShakunVaid@users.noreply.github.com>
2025-07-14 13:15:44 +0100

    Update SP_summay.sql

--- Script/Storeprocedures/SP_summay.sql
+++ Script/Storeprocedures/SP_summay.sql
@@ -13,3 +13,19 @@ BEGIN
 END
 
 EXECUTE customersummary @Country = 'Germany'
+
+--------------Stored Procedure to select customer summary based upon country filter with default value--------------------------
+
+Use SalesDB
+
+CREATE PROCEDURE customersummary @Country NVARCHAR(20)='USA'
+
+AS
+BEGIN
+	SELECT COUNT(*) TotalCustomer,
+	AVG(Score) AverageScore
+	FROM Sales.Customers
+	WHERE Country = @Country
+END
+
+EXECUTE customersummary 
