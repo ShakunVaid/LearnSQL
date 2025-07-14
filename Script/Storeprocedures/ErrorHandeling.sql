302e81f280a85e81aee76b6b4763d4de7559f3ce
ShakunVaid <98100058+ShakunVaid@users.noreply.github.com>
2025-07-14 14:34:10 +0100

    Update ErrorHandeling.sql

--- Script/Storeprocedures/ErrorHandeling.sql
+++ Script/Storeprocedures/ErrorHandeling.sql
@@ -9,12 +9,14 @@
 CREATE OR ALTER PROCEDURE customersummary @Country NVARCHAR(20)='USA'
 AS
 BEGIN
--------Error Handeling with TRY and Catch, started try block-----------------
-BEGIN TRY
+	-- ==========================================================================
+	-- Error Handeling with TRY and Catch, started try block
+	-- ==========================================================================
+	BEGIN TRY
 		DECLARE @totalcustomer INT, @avgscore FLOAT;
-
-
-		---PREPARE AND CLEAN DATA
+		-- =======================================================================
+		-- STEP1 : PREPARE AND CLEAN DATA
+		-- =======================================================================
 		IF EXISTS (SELECT 1 FROM sales.Customers WHERE Score IS NULL AND Country = @Country)
 		BEGIN
 			PRINT('Updating NULL to 0');
@@ -22,42 +24,46 @@ BEGIN TRY
 			SET Score = 0	
 			WHERE Score IS NULL AND Country = @Country
 		END
-
 		ELSE
 		BEGIN
 			PRINT('No null found');
 		END;
-
-
-
-		-----GENERATING REPORT
-			SELECT 
+		-- ==============================================================================
+		-- STEP2 : GENERATING SUMMARY REPORT
+		-- ==============================================================================
+		-- Calculate total customer and score for filtered country 
+		SELECT 
 			@totalcustomer = COUNT(*),
 			@avgscore =	AVG(Score)
-			FROM Sales.Customers
-			WHERE Country = @Country;
+		FROM Sales.Customers
+		WHERE Country = @Country;
+		PRINT 'Total customer from ' + @Country + '=' + CAST(@totalcustomer AS NVARCHAR);
+		PRINT 'Average Score  from ' + @Country + '=' + CAST(@avgscore AS NVARCHAR);
 
-			PRINT 'Total customer from ' + @Country + '=' + CAST(@totalcustomer AS NVARCHAR);
-			PRINT 'Average Score  from ' + @Country + '=' + CAST(@avgscore AS NVARCHAR);
-
-
-		---Total Order and total sales-------------------------
-			SELECT COUNT(O.OrderID) TotalOrders,
-			Sum(O.Sales) TotalSales,
+		-- Calculate  Total Order and total sales for filtered country
+		SELECT
+			COUNT(O.OrderID) TotalOrders,
+			Sum(O.Sales) TotalSales
 			--1/0
-			FROM Sales.Orders as O
-			LEFT JOIN Sales.Customers as C
-			ON O.CustomerID=C.CustomerID
-			WHERE C.Country = @country;
-END TRY
-BEGIN CATCH
-	PRINT('Error Occured');
-	PRINT('Error Message :' + ERROR_MESSAGE());
-	PRINT( 'Error Number :' + CAST(ERROR_NUMBER() AS NVARCHAR));
-	PRINT ('Error Line :' + CAST(ERROR_LINE() AS NVARCHAR));
-	PRINT('Error Procudere :' + ERROR_PROCEDURE());
-END CATCH
+		FROM Sales.Orders as O
+		LEFT JOIN Sales.Customers as C
+		ON O.CustomerID=C.CustomerID
+		WHERE C.Country = @country;
+	END TRY
+	BEGIN CATCH
+		-- =========================================================================
+		-- ERROR HANDELING
+		-- =========================================================================
+		PRINT('Error Occured');
+		PRINT('Error Message :' + ERROR_MESSAGE());
+		PRINT( 'Error Number :' + CAST(ERROR_NUMBER() AS NVARCHAR));
+		PRINT ('Error Line :' + CAST(ERROR_LINE() AS NVARCHAR));
+		PRINT('Error Procudere :' + ERROR_PROCEDURE());
+	END CATCH
 END
 GO
-----Execute stored procedure
+-- ====================================================================================
+-- Execute stored procedure
+-- =====================================================================================
+EXECUTE customersummary
 EXECUTE customersummary @Country ='Germany'
